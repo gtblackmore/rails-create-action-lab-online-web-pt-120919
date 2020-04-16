a606158b088d04e2a7450a81e8fe94ed3f8e588b
George Blackmore <gtblackmore@gmail.com>
2020-04-16 16:51:20 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,11 +12,10 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(
-      {
-        first_name: params[:first_name],
-        last_name: params[:last_name]
-      }
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.created_at = 
     )
     
     redirect_to student_path(@student)
